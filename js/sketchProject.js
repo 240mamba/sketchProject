922c4720d653c6bc5d0df0c397cbbace1ce6e9bf
MambaG <240mamba@gmail.com>
2017-07-21 07:22:25 -0500

    Udpated the ui and created a scalable grid

--- js/sketchProject.js
+++ js/sketchProject.js
@@ -1,5 +1,24 @@
+var userPixel = 2;
+var gridWidth = 1/userPixel * 100;
+var gridWidthPercent = gridWidth + "%";
+
 $(document).ready(function () {
-         for (var i = 1; i <= 256; i++) {
-             $('.container').append('<div></div>');
+
+    var pixelCount = userPixel * userPixel;
+         for (var i = 1; i <= pixelCount; i++) {
+             $('.container').append('<div class="squares"></div>');
          }
+         $('.squares').css('width', gridWidthPercent);
+         $('.squares').css('height', gridWidthPercent);
+         
      });
+
+
+
+var gridString = gridWidthPercent.toString();
+
+var styles = {"width":gridWidthPercent, "height":gridWidthPercent};
+
+
+
+console.log(gridString);
