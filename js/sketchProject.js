0a7cedf771c812ce86d936849416d175ada94d95
MambaG <240mamba@gmail.com>
2017-07-22 15:13:59 -0500

    Finalized and updated to the Pixel Trail Project to better represent my creation.  https://jsfiddle.net/2e7ctpsb/

--- js/sketchProject.js
+++ js/sketchProject.js
@@ -1,24 +1,37 @@
-var userPixel = 2;
-var gridWidth = 1/userPixel * 100;
-var gridWidthPercent = gridWidth + "%";
 
 $(document).ready(function () {
 
+function  clear(){
+
+    //List of variables for function
+    var userInput = $('#userInput').val();
+    $('#userInput').val(userInput);
+    var userPixel= parseInt(userInput);
+    var gridWidth = 1/userPixel * 100;
+    var gridWidthPercent = gridWidth + "%";
+    var c = $('.container');
     var pixelCount = userPixel * userPixel;
-         for (var i = 1; i <= pixelCount; i++) {
-             $('.container').append('<div class="squares"></div>');
+    //Actual loop function with basic javascript to speed up higher inputs
+    for (var i = 1; i <= pixelCount; i++) {
+            var test = document.createElement('div');
+            test.className="squares";
+            c.append(test);
          }
-         $('.squares').css('width', gridWidthPercent);
-         $('.squares').css('height', gridWidthPercent);
-         
-     });
+
+    //Sets the width and height to the new grid squares
+     $('.container div').css('width', gridWidthPercent);
+     $('.container div').css('height', gridWidthPercent);
+
+    };
 
 
+     //Clear button calls function
+     $('.clear').click(clear);
 
-var gridString = gridWidthPercent.toString();
-
-var styles = {"width":gridWidthPercent, "height":gridWidthPercent};
-
-
-
-console.log(gridString);
+     //Enter button calls function
+     $('#userInput').keypress(function(e) {
+    if(e.which == 13) {
+        clear();
+    }
+  });
+});
